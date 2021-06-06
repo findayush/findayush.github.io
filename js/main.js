23202d05f832c0f1faa6335fc01228e94a01f99d
Ayush Kumar <47985276+findayush@users.noreply.github.com>
2021-06-06 14:55:18 +0530

    Creating balls animation generator

--- js/main.js
+++ js/main.js
@@ -40,7 +40,52 @@
 	// Scrollax
    $.Scrollax();
 
+//Balls Animation generation
+  // Some random colors
+  const colors = ["#3CC157", "#2AA7FF", "#7f7f7f", "#FFBD39", "#ffffff"];
 
+  const numBalls = 50;
+  const balls = [];
+  const genearteBalls = (idArray) => {
+    for (let id of idArray) {
+      for (let i = 0; i < numBalls; i++) {
+        let ball = document.createElement("div");
+        ball.classList.add("ball");
+        ball.style.background =
+          colors[Math.floor(Math.random() * colors.length)];
+        ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+        ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+        ball.style.transform = `scale(${Math.random()})`;
+        ball.style.width = `${Math.random()}em`;
+        ball.style.height = ball.style.width;
+
+        balls.push(ball);
+        document.getElementById(id).append(ball);
+      }
+    }
+  };
+  genearteBalls(["slider1", "slider2"]);
+  // Keyframes
+  balls.forEach((el, i, ra) => {
+    let to = {
+      x: Math.random() * (i % 2 === 0 ? -11 : 11),
+      y: Math.random() * 12,
+    };
+
+    let anim = el.animate(
+      [
+        { transform: "translate(0, 0)" },
+        { transform: `translate(${to.x}rem, ${to.y}rem)` },
+      ],
+      {
+        duration: (Math.random() + 1) * 2000, // random duration
+        direction: "alternate",
+        fill: "both",
+        iterations: Infinity,
+        easing: "ease-in-out",
+      }
+    );
+  });
 
    // Burger Menu
 	var burgerMenu = function() {
